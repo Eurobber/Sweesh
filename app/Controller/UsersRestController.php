4fbd463e4f1d5b7b125ce0adc3a303dd5d26b3ba
Stoumfou <alexis.pambourg@gmail.com>
2017-01-28 18:32:20 +0100

    Ajout UserRestController

---
+++ app/Controller/UsersRestController.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: alexis
+ * Date: 28/01/2017
+ * Time: 18:22
+ */
+
+App::uses('AppController', 'Controller');
+
+
+class UsersRestController extends AppController {
+
+    public $uses = array('User');
+    public $helpers = array('Html', 'Form');
+    public $components = array('RequestHandler');
+
+    public function beforeFilter()
+    {
+        parent::beforeFilter();
+        $this->Auth->allow('index', 'view');
+    }
+
+    public function isAuthorized($user = null)
+    {
+        return true;
+    }
+
+    public function index() {
+        $users = $this->User->find('all');
+        $this->set(array(
+            'users' => $users,
+            '_serialize' => array('users')
+        ));
+    }
+
+    public function view($id) {
+        $user = $this->User->findById($id);
+        $this->set(array(
+            'user' => $user,
+            '_serialize' => array('user')
+        ));
+    }
+}
+?>
